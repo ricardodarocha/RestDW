16a9c26ef6f3d2749a7fbaf65078cca8f93bcd1e
Ricardo da Rocha <ricardodarocha@outlook.com>
2020-01-22 13:14:30 -0300

    Add files via upload
    Amazing Customizable Listview sample

---
+++ App/uListBox.pas
@@ -0,0 +1,49 @@
+unit uListBox;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Objects, FMX.ListBox, FMX.Layouts, FMX.Controls.Presentation, FMX.StdCtrls;
+
+type
+  TForm2 = class(TForm)
+    ListBox1: TListBox;
+    ListBoxItem2: TListBoxItem;
+    Circle2: TCircle;
+    Circle3: TCircle;
+    Label2: TLabel;
+    Label4: TLabel;
+    ListBoxItem1: TListBoxItem;
+    Circle1: TCircle;
+    Circle4: TCircle;
+    Label1: TLabel;
+    Label3: TLabel;
+    ListBoxItem3: TListBoxItem;
+    Circle5: TCircle;
+    Circle6: TCircle;
+    Label5: TLabel;
+    Label6: TLabel;
+    ListBoxItem5: TListBoxItem;
+    Rectangle2: TRectangle;
+    Rectangle1: TRectangle;
+    ListBoxItem6: TListBoxItem;
+    Rectangle3: TRectangle;
+    Rectangle4: TRectangle;
+    ListBoxItem7: TListBoxItem;
+    Rectangle5: TRectangle;
+    Rectangle6: TRectangle;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.fmx}
+
+end.
